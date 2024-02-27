47c17404b5cdd4e034629ba94d45f1700b1ea18d
sami gharsallah <samigharsallah01@gmail.com>
2024-02-27 10:51:18 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../custom.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
